785a530008b9db136cb13cfdfc32e83302c212fd
a_zuzu <elianwebster0@gmail.com>
2023-04-07 00:17:43 -0500

    update

---
+++ log/v1.2/README.md
@@ -0,0 +1,7 @@
+## Changelog
+
+v1.2
+
+* Se elimina las siguientes funciones: ``get_ListName`` y ``get_Posicion``.
+* Se mejora la logica de las siguientes funciones: ``get_AnimeLink``, ``get_AnimeName``, ``get_AnimeEpisode``, ``get_state_episode`` y ``get_restTime``.
+* Se agrega la función: ``search_Anime``.
--- main.py
+++ main.py
@@ -4,98 +4,89 @@ import re
 import datetime
 from notifypy import Notify
 
-
+# Obtenemos todo el código HTML
 def request(url):
     soup = BeautifulSoup(requests.get(url).text, "html.parser")
     return soup
 
+# Obtenemos un link completo (ej. /anime/{id} 
 def complete_link(url): # We return the complete link
     url_input = "https://notify.moe{}".format(url)
     return url_input
 
-def get_ListName(url):
+def search_Anime(name):
     try:
-        soup = request(url)
-        anime_names = soup.select("div.explore-anime a img")
-        list_names = []
-        for get_names in anime_names:
-            list_names.append(get_names.get("alt"))
-        return list_names
-    except:
-        return "Error 40: Fallo al obtener la lista de animes."
-
-def get_Posicion(list_animes, inputAnime):
+        soup = request("https://notify.moe/explore")
+        anime_names = soup.select("div.image-title")
+        list_of_animes = ()
+        for names in anime_names:
+            if name.lower() in names.text.lower():  
+                list_of_animes += (names.text,)
+        return list_of_animes
+    except Exception:
+        return f"Hubo un error: {Exception}"
+    
+# Obtenemos la {id} del anime
+def get_AnimeLink(inputAnime): # Obtain the sought anime link (ex: /anime/asGd86)
     try:
-        position = None
-        for i in range(len(list_animes)):
-            if inputAnime.lower() in list_animes[i].lower():
-                position = i+1
-        if position is None:
-            raise ValueError
-        return position
-    except TypeError:
-        return "Error 11: El dato ingresado no es de tipo string."
-    except:
-        return "Error 41: Fallo al encontrar el anime"
-
-def get_AnimeLink(url, position): # Obtain the sought anime link (ex: /anime/asGd86)
-    try:
-        soup = request(url)
+        soup = request("https://notify.moe/explore")
         anime_links = soup.select("div.anime-grid-cell a")
-        list_links = []
-        for gets_links in anime_links:
-            list_links.append(gets_links.get("href"))
-        return list_links[position-1]
+        dic_links = {}
+        for anime_link in anime_links:
+            dic_links[anime_link.find('img')['alt'].lower()] = anime_link['href']
+        anime_name = next((key for key in dic_links.keys() if inputAnime in key.lower()), None)
+        return dic_links.get(anime_name) 
     except:
         return "Error 42: Fallo al obtener el link del anime."
 
-def get_Name(link):
+# Obtenemos el nombre (refactor)
+def get_AnimeName(link):
     try:
         soup = request(complete_link(link))
-        anime_name = soup.select("div.anime-info h1")
-        for name in anime_name:
-            anime_name = name.text
-        return anime_name
+        return soup.find("h1", class_="anime-title mountable").text
     except:
         return "Error 43: Fallo al obtener el nombre."
-    
-def get_Episode(url): # Obtain the number of the chapter
+
+# def get_AnimeEpisode(url, episode=None):
+#     try:
+#         soup = request(complete_link(url))
+#         latest_episode = soup.select("div.episodes a[data-available=true]")
+#         count = len(latest_episode)
+#         if episode is not None:
+#             episode_number = count - episode
+#             if episode_number >= 0:
+#                 return episode_number + 1
+#             else:
+#                 return "Error: Episodio no disponible"
+#         else:
+#             return count
+#     except:
+#         return "Error: Fallo al obtener el episodio."
+
+def get_AnimeEpisode(url):
     try:
         soup = request(complete_link(url))
-        latest_episode = soup.select("div.episodes a")
-        count = 0
-        for episode in latest_episode:
-            if episode.get("data-available") != "false":
-                count += 1
+        latest_episode = soup.select("div.episodes a[data-available=true]")
+        count = len(latest_episode)
         return count
     except:
         return "Error 44: Fallo al obtener el episodio."
-
+    
 def get_state_episode(url): # Obtain episode state
     try:
         soup = request(complete_link(url))
-        latest_episode = soup.select("div.episodes a")
-        state_episode = "true"
-        for episode in latest_episode:
-            available = episode.get("da a-available")
-            span = episode.find("span", class_="episode-title")
-            if available == "true" and span.get_text() == "-":
-                state_episode = "false"
-                break
-            elif available == "false":
-                state_episode = available
-                break
-        return state_episode
+        latest_episode = soup.select_one("div.episodes a[data-available='true'] span.episode-title")
+        if latest_episode and latest_episode.get_text() == "-":
+            return True
+        else:
+            return False
     except:
         return "Error 45: Fallo al obtener el estado del episodio."
 
 def get_restTime(url, episode):
     try:
         soup = request(complete_link(url))
-        timer = soup.select("div.episodes a")
-        dates = []
-        for datatime_anime in timer:
-            dates.append(datatime_anime.text)
+        dates = [elem.text for elem in soup.select("div.episodes a")]
         match = re.search(r'.*(\w{3}, \d{2} \w{3} \d{4})', dates[episode])
         if match:
             date_str = match.group(1)
@@ -108,43 +99,27 @@ def get_restTime(url, episode):
     except:
         return "Error 46: Fallo al obtener el tiempo restante."
 
-def search_Anime(name,list_animes): # Cambiar estructura 
-    resultados = []
-    for n in list_animes:
-        if name.lower() in n.lower():
-            resultados.append(n)
-    if resultados:
-        return resultados
-    else:
-        return None
-
 # enviar notificacion
 def send_notification(title, message):
-    # icon_file = r"imagen.jpeg"
     notification = Notify()
     notification.title = title
     notification.message = message
-    # notification.icon = icon_file
     notification.send()
 
 
-def main():
-    try:
-        inputAnime = input("What anime is going to be notifiyed? \n") # Anime to search
-        url_base = "https://notify.moe/explore" # web page
-        Listname = get_ListName(url_base) # Obtain the naime list
-        position = get_Posicion(Listname,inputAnime) # We shearch for the position of the anime
-        links = get_AnimeLink(url_base,position) # We obtain the anime link (/anime/id)
-        anime_name = get_Name(links) # We obtain the name
-        episode_number = get_Episode(links) # Episode to air / Last espisode
-        state = get_state_episode(links) # Espisode state
-        time = get_restTime(links, episode_number) # Obtain the rest time 
-    
-        if state == "false":
-            send_notification("Anime Notification", f"Episode {episode_number+1} of {anime_name} is avaiable.{time}")
-        else:
-            send_notification("Anime Notification", f"Episode {episode_number+1} of {anime_name} it`s not avaiable, it`s going to air in {time} days.")
-    except requests.exceptions.RequestException as exceptMessage:
-        raise("[378 Error]:", exceptMessage)
+# def main():
+#     try:
+#         inputAnime = input("What anime is going to be notifiyed? \n") # Anime to search
+#         links = get_AnimeLink(inputAnime)
+#         anime_name = get_AnimeName(links)
+#         episode_number = get_AnimeEpisode(links)
+#         state = get_state_episode(links)
+#         time = get_restTime(links, episode_number)
+#         if state == False:
+#             send_notification("Anime Notification", f"Episode {episode_number+1} of {anime_name} is avaiable.{time}")
+#         else:
+#             send_notification("Anime Notification", f"Episode {episode_number+1} of {anime_name} it`s not avaiable, it`s going to air in {time} days.")
+#     except requests.exceptions.RequestException as exceptMessage:
+#         raise("[378 Error]:", exceptMessage)
         
-main()
+# main()
